e09759fc984c5e8c4f15176907f8cc55b7dbddf5
Daniela Pech <randakova.daniela@gmail.com>
2025-05-27 19:52:16 +0200

    pridani dark mode, ukladani do local storrage

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const toggleSwitch = document.querySelector(
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); //uložení preferencí uživatele
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light'); //uložení preferencí uživatele
   }
 };
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme')
+  ? localStorage.getItem('theme')
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
